b1df0041dc0e21d864f8b1a6f3cbe9afe998df2f
Gabriel <gabrielmesquita0503@gmail.com>
2024-06-14 21:19:12 -0300

    ContaTerminal

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Por favor, digite o número da Conta");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da Agencia");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o nome do Titular");
+        String titular = scanner.nextLine();
+
+        System.out.println("Por favor, digite o Saldo");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá "+titular+", obrigado por criar uma conta em nosso banco, sua agência é"+agencia+", conta "+numero+" e seu saldo "+saldo+" já está disponível para saque");
     }
 
 }
